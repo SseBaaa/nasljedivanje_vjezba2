067e92bc0c8e8b09d1bb9fe9dd8937243d2e10fe
SseBaaa <89750702+SseBaaa@users.noreply.github.com>
2022-02-15 13:02:29 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasljeđivanje_vjezba2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Proceduralni x = new Proceduralni();
+            Objektni y = new Objektni();
+            Funkcionalni z = new Funkcionalni();
+
+            Console.WriteLine("Unesi x: ");
+            x.Postotak =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Unesi y: ");
+            y.Postotak = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Unesi z: ");
+            z.Postotak = Convert.ToInt32(Console.ReadLine());
+
+            double zbroj = x.Postotak + y.Postotak + z.Postotak;
+
+            if(zbroj!= 100)
+            {
+                Console.WriteLine("Neispravan unos podataka");
+            }
+            if(zbroj == 100)
+            {
+                if(x.Postotak > y.Postotak && x.Postotak > z.Postotak)
+                {
+                    Console.WriteLine("Najveci postotak ima procedurarlno sa: " + x.Postotak);
+                }
+                if (y.Postotak > x.Postotak && y.Postotak > z.Postotak)
+                {
+                    Console.WriteLine("Najveci postotak ima Objektno sa: " + y.Postotak);
+                }
+                if (z.Postotak > y.Postotak && z.Postotak > x.Postotak)
+                {
+                    Console.WriteLine("Najveci postotak ima Funkcionalno sa: " + z.Postotak);
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+
+    class ProgramskiJezik
+    {
+        double postotak;
+
+        public double Postotak { get => postotak; set => postotak = value; }
+    }
+
+    class Proceduralni : ProgramskiJezik
+    {
+
+    }
+
+    class Objektni : ProgramskiJezik
+    {
+
+    }
+
+    class Funkcionalni : ProgramskiJezik
+    {
+
+    }
+}
